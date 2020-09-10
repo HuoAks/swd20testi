292284f986acc7865ac84177eb56c3a3dc3afb87
HuoAks <ahuovi@gmail.com>
2020-09-10 15:14:23 +0300

    projekti luotu

---
+++ src/test/java/hh/swd20/Gittest/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.swd20.Gittest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
